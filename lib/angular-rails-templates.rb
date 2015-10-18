f6b7fc39c409226f012b5558c7dc4e49ef2b6cb1
Денис Епишкин <nottewae@iloud.com>
2015-10-18 14:38:01 +0300

    errors fix

--- lib/angular-rails-templates.rb
+++ lib/angular-rails-templates.rb
@@ -1,5 +1,5 @@
 require 'angular-rails-templates/engine'
-require 'my_gem/railtie' if defined?(Rails)
+require 'angular-rails-templates/railtie' if defined?(Rails)
 module AngularRailsTemplates
   autoload :Template, 'angular-rails-templates/template'
   autoload :VERSION,  'angular-rails-templates/version'
